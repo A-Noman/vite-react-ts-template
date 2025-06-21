040ad3e131a7b54f4a1a0d5e792313cfa4dcbfda
Asad Noman <sh.asadnoman@gmail.com>
2025-06-21 13:49:46 -0400

    INV-32 Connected frontend to backend - TestComponent that runs fetch to the api base url and prints the response in the UI

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { setRoutes } from './routes/index.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// This line allows CORS from Vite (default port 5173)
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+);
 
 // Middleware
 app.use(bodyParser.json());
